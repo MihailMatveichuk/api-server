d70584010e739fb2ff5f010b8ddbea14080635e8
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2024-06-21 21:22:39 +0300

    feat: add models

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, async () => {
+      console.log(`Server is running on port ${PORT}`);
+      console.log('Database connected');
+    });
+  } catch (error) {
+    console.log('Unable to connect to the database');
+    console.log(error);
+  }
+};
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-
-app.listen(PORT, async () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log("Database connected");
-})
+start();
